1b11225ba3831cb1d06010b80e52cf1655581087
권민영 <918liter@gmail.com>
2023-03-26 13:08:09 +0900

    feat: useRenderCells hook 분리

--- src/components/RenderCells.tsx
+++ src/components/RenderCells.tsx
@@ -1,8 +1,6 @@
 import React from "react";
-import { startOfMonth, endOfMonth, startOfWeek, endOfWeek } from "date-fns";
-import { format, addMonths, subMonths } from "date-fns";
-import { isSameMonth, isSameDay, addDays, parse } from "date-fns";
 import styled from "styled-components";
+import useRenderCells from "../hooks/useRenderCells";
 
 
 type RenderCellsProps = {
@@ -17,30 +15,9 @@ export default function RenderCells ({
     selectedDate,
     onDateClick,
   }: RenderCellsProps) {
-    const monthStart = startOfMonth(currentMonth);
-    const monthEnd = endOfMonth(monthStart);
-    const startDate = startOfWeek(monthStart);
-    const endDate = endOfWeek(monthEnd);
-  
-    const rows = [];
-    let days = [];
-    let day = startDate;
-    let formattedDate = "";
-  
-    while (day <= endDate) {
-      for (let i = 0; i < 7; i++) {
-        formattedDate = format(day, "d");
-        //   해당 달의 날짜가 아니면 date에 null, 맞다면 날짜 그대로 객체에 들어감
-        format(currentMonth, "M") !== format(day, "M")
-          ? days.push({ date: null, menu: "" })
-          : days.push({ date: formattedDate, mainMenu: "", soup: "", side: "" });
-        day = addDays(day, 1);
-      }
-  
-      rows.push(days);
-      days = [];
-    }
-  
+
+const {rows, day} = useRenderCells({currentMonth})
+
     return (
     <Body className="body">      
       {rows.map((row) => (
