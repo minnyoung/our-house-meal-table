43507fe46b6cc9e3d7f787ace5a698a7f2f8d7cf
FROM-minyoung <918liter@gmail.com>
2023-03-29 20:19:20 +0900

    style: 메뉴 navbar 분류 버튼 style 적용

--- src/components/MenuLayout.tsx
+++ src/components/MenuLayout.tsx
@@ -8,10 +8,32 @@ export default function Menu() {
   const [menuState, setMenuState] = useState("");
   return (
     <Container>
-      <div>메뉴를 선택해주세요</div>
-      <button onClick={() => setMenuState("MAIN")}>메인메뉴</button>
-      <button onClick={() => setMenuState("SOUP")}>국거리</button>
-      <button onClick={() => setMenuState("SIDE")}>반찬</button>
+      <ButtonContainer>
+        <MenuButton
+          onClick={() => setMenuState("MAIN")}
+          menuType="MAIN"
+          menuState={menuState}
+          color="#65c2c7"
+        >
+          메인메뉴
+        </MenuButton>
+        <MenuButton
+          onClick={() => setMenuState("SOUP")}
+          menuType="SOUP"
+          menuState={menuState}
+          color="#ef9fbc"
+        >
+          국거리
+        </MenuButton>
+        <MenuButton
+          onClick={() => setMenuState("SIDE")}
+          menuType="SIDE"
+          menuState={menuState}
+          color="#edae3a"
+        >
+          반찬
+        </MenuButton>
+      </ButtonContainer>
       <div>
         {menuState === "MAIN" ? (
           <MainDish />
@@ -27,4 +49,24 @@ export default function Menu() {
 
 const Container = styled.div`
   width: 20rem;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  margin-top: 20px;
+`;
+
+const MenuButton = styled.button<{ menuType: string; menuState: string }>`
+  margin: 4px;
+  flex: 1 1 auto;
+  height: 3em;
+  background-color: ${({ menuType, menuState, color }) =>
+    menuType === menuState ? color : "transparent"};
+  border: none;
+  border-radius: 60px;
+  :hover {
+    background-color: ${({ color }) => color};
+    cursor: pointer;
+    transition: all 0.2s ease;
+  }
 `;
