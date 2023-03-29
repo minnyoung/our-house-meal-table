831b4393deed82be2ab257ad6218dab8872c5a6a
FROM-minyoung <918liter@gmail.com>
2023-03-29 16:49:16 +0900

    style: 요일 style 수정

--- src/components/RenderDays.tsx
+++ src/components/RenderDays.tsx
@@ -2,31 +2,31 @@ import React from "react";
 import styled from "styled-components";
 
 // 요일
-export default function RenderDays () {
-    const dates = ["일", "월", "화", "수", "목", "금", "토"];
-  
-    return (
-      <Days className="days row weekends">
-        {dates.map((date, index) => (
-          <DaysCol key={index}>{date}</DaysCol>
-        ))}
-      </Days>
-    );
-  };
-  
-  const Days = styled.div`
+export default function RenderDays() {
+  const dates = ["일", "월", "화", "수", "목", "금", "토"];
+
+  return (
+    <Days className="days row weekends">
+      {dates.map((date, index) => (
+        <DaysCol key={index} color={date}>
+          {date}
+        </DaysCol>
+      ))}
+    </Days>
+  );
+}
+
+const Days = styled.div`
   width: 100%;
   height: fit-content;
-
+  padding: 6px 0;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
 
   font-weight: 600;
-  font-size: 0.65em;
-  padding: 2px;
-  color: #686868;
+  font-size: 0.75em;
 `;
 
 const DaysCol = styled.div`
@@ -36,4 +36,7 @@ const DaysCol = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  color: ${({ color }) =>
+    color === "토" ? "#ff5151" : color === "일" ? "#2631ff" : "#686868"};
 `;
