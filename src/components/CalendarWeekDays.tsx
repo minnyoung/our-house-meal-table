a7f7c601ef1b7d7f520d55a6c5fadf22b2a484a1
minnyoung <918liter@gmail.com>
2023-05-09 00:13:43 +0900

    style: CalendarWeekDays.tsx css 수정

--- src/components/CalendarWeekDays.tsx
+++ src/components/CalendarWeekDays.tsx
@@ -1,42 +1,39 @@
 import React from "react";
 import styled from "styled-components";
 
-// 요일
 export default function CalendarWeekDays() {
   const weekDays = ["일", "월", "화", "수", "목", "금", "토"];
 
   return (
-    <Days className="days row weekends">
+    <S.Container>
       {weekDays.map((weekDay, index) => (
-        <DaysCol key={`weekDay-${index}`} weekDay={weekDay}>
+        <S.WeekDayWrapper key={`weekDay-${index}`} weekDay={weekDay}>
           {weekDay}
-        </DaysCol>
+        </S.WeekDayWrapper>
       ))}
-    </Days>
+    </S.Container>
   );
 }
 
-const Days = styled.div`
-  width: 100%;
-  height: fit-content;
-  padding: 6px 0;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-
-  font-weight: 600;
-  font-size: 0.75em;
-`;
-
-const DaysCol = styled.div<{ weekDay: string }>`
-  width: 12.9%;
-  height: 100%;
-
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-
-  color: ${({ weekDay }) =>
-    weekDay === "일" ? "#ff5151" : weekDay === "토" ? "#2631ff" : "#686868"};
-`;
+const S = {
+  Container: styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 6px 0;
+    width: 100%;
+    height: fit-content;
+    font-weight: 600;
+    font-size: 0.75em;
+  `,
+  WeekDayWrapper: styled.div<{ weekDay: string }>`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 12.9%;
+    height: 100%;
+    color: ${({ weekDay }) =>
+      weekDay === "일" ? "#ff5151" : weekDay === "토" ? "#2631ff" : "#686868"};
+  `,
+};
