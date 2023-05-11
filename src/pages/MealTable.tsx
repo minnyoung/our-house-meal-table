293cfea2043c98ac8dbaf4efc68c48c06ee915b9
minnyoung <918liter@gmail.com>
2023-05-11 19:18:30 +0900

    feat: /mealTable 접속 시 유저 로그인 인증 hook 추가(#5)

--- src/pages/MealTable.tsx
+++ src/pages/MealTable.tsx
@@ -1,8 +1,14 @@
+import { useEffect } from "react";
 import Calendar from "../components/Calendar";
 import MenuLayout from "../components/MenuLayout";
 import styled from "styled-components";
+import useUserAuthFunction from "../hooks/useUserAuthFunction";
 
 export default function MealTable() {
+  const { checkUserAuthentication } = useUserAuthFunction();
+
+  useEffect(checkUserAuthentication, []);
+
   return (
     <S.Container>
       <Calendar />
