e37735a81a84ea4b3b79da3c8334b3c0283f3266
minnyoung <918liter@gmail.com>
2023-05-11 18:25:40 +0900

    feat: 파이어베이스로 google login 구현(#5)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,49 @@
+import { useEffect, useState } from "react";
+
+import {
+  signInWithPopup,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { auth } from "../firebase-config";
+import { useNavigate } from "react-router";
+
 export default function Home() {
+  const navigate = useNavigate();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // logged in
+        setIsLoggedIn(true);
+        navigate("/mealTable");
+      } else {
+        // logged out
+        setIsLoggedIn(false);
+        navigate("/");
+      }
+    });
+  }, []);
+
+  async function clickGooglelogin() {
+    const provider = new GoogleAuthProvider();
+    try {
+      await signInWithPopup(auth, provider);
+    } catch (error) {
+      console.log("로그인 시 에러", error);
+    }
+  }
+
   return (
-    <div>
-      <div>우리집 식단표</div>
-      <div>로그인</div>
-      <button>로그인</button>
-    </div>
+    <>
+      {isLoggedIn && (
+        <div>
+          <div>우리집 식단표</div>
+          <div>로그인</div>
+          <button onClick={clickGooglelogin}>로그인</button>
+        </div>
+      )}
+    </>
   );
 }
