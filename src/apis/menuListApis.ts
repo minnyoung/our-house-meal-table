0516725ddfdcb6056afe4c16e4d00a10c9f0fdd8
minnyoung <918liter@gmail.com>
2023-05-12 01:17:00 +0900

    feat: 파이어베이스로 저장한 사용자의 식단표 불러오기 구현(#5)

--- src/apis/menuListApis.ts
+++ src/apis/menuListApis.ts
@@ -21,3 +21,14 @@ export async function setUserMenuList(menuList: MenuType[], userId: string) {
     console.error("등록 중 에러가 발생했습니다.", e);
   }
 }
+
+export async function getUserMenuList(userId: string) {
+  const docData = doc(firestore, "users", userId);
+  const querySnapshot = await getDoc(docData);
+  try {
+    const data = await querySnapshot.data();
+    return data;
+  } catch (e) {
+    console.log(e);
+  }
+}
--- src/pages/MealTable.tsx
+++ src/pages/MealTable.tsx
@@ -3,11 +3,23 @@ import Calendar from "../components/Calendar";
 import MenuLayout from "../components/MenuLayout";
 import styled from "styled-components";
 import useUserAuthFunction from "../hooks/useUserAuthFunction";
+import { getUserMenuList } from "../apis/menuListApis";
+import { mainMenuStore, userIdStore } from "../store/MainStore";
 
 export default function MealTable() {
   const { checkUserAuthentication } = useUserAuthFunction();
+  const { userId } = userIdStore();
+  const { setMenuList } = mainMenuStore();
 
-  useEffect(checkUserAuthentication, []);
+  async function setMealTable() {
+    const menuList = await getUserMenuList(userId);
+    setMenuList(menuList?.menuList);
+  }
+
+  useEffect(() => {
+    checkUserAuthentication();
+    setMealTable();
+  }, []);
 
   return (
     <S.Container>
