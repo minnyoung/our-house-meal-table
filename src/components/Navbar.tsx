1dc0ee7685908fce02dd46193dd3bb35ff2d78fa
minnyoung <918liter@gmail.com>
2023-05-13 01:47:50 +0900

    feat: 로그아웃 기능 구현(#5)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,24 @@
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase-config";
+import { removeUid } from "../utils/uid";
+
 export default function Navbar() {
+  function logOut() {
+    signOut(auth)
+      .then(() => {
+        removeUid();
+      })
+      .catch((error) => {
+        console.log("로그아웃 중 에러가 발생했습니다.", error);
+      });
+  }
+
   return (
     <div>
       <div>네비게이션바</div>
-      <button type="button">로그아웃</button>
+      <button type="button" onClick={logOut}>
+        로그아웃
+      </button>
     </div>
   );
 }
