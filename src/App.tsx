993078cab7116413eeac8916da5e428e8685c011
minnyoung <918liter@gmail.com>
2023-05-27 14:09:26 +0900

    style: GlobalStyle 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Router from "./router";
+import { GlobalStyle } from "./styles/GlobalStyle";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <Router />
     </div>
   );
