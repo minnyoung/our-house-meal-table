42b6f9dbeb2aa5583ea6c333873d4db39f342d19
minnyoung <918liter@gmail.com>
2023-05-27 22:25:46 +0900

    feat: 모달 메뉴 삭제버튼 컴포넌트 분리(#6)

--- src/components/MenuModal.tsx
+++ src/components/MenuModal.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { userMenuStore } from "../store/userMenuStore";
+import ModalMenuDeleteButton from "./element/ModalMenuDeleteButton";
 
 type MenuModalType = {
   date: string;
@@ -44,27 +45,9 @@ export default function MenuModal({ date, setIsOpenMenuModal }: MenuModalType) {
                 <td>{dayMenuList.userMainMenu}</td>
                 {dayMenuList.userMainMenu && (
                   <td>
-                    <S.MenuDeleteButton
-                      onClick={() => deleteUserMenu("userMainMenu")}
-                    >
-                      <svg
-                        className="feather feather-trash-2"
-                        fill="none"
-                        height="18"
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        viewBox="0 0 24 24"
-                        width="18"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <polyline points="3 6 5 6 21 6" />
-                        <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
-                        <line x1="10" x2="10" y1="11" y2="17" />
-                        <line x1="14" x2="14" y1="11" y2="17" />
-                      </svg>
-                    </S.MenuDeleteButton>
+                    <ModalMenuDeleteButton
+                      deleteUserMenu={() => deleteUserMenu("userMainMenu")}
+                    />
                   </td>
                 )}
               </tr>
@@ -73,27 +56,9 @@ export default function MenuModal({ date, setIsOpenMenuModal }: MenuModalType) {
                 <td>{dayMenuList.userSoup}</td>
                 {dayMenuList.userSoup && (
                   <td>
-                    <S.MenuDeleteButton
-                      onClick={() => deleteUserMenu("userSoup")}
-                    >
-                      <svg
-                        className="feather feather-trash-2"
-                        fill="none"
-                        height="18"
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        viewBox="0 0 24 24"
-                        width="18"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <polyline points="3 6 5 6 21 6" />
-                        <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
-                        <line x1="10" x2="10" y1="11" y2="17" />
-                        <line x1="14" x2="14" y1="11" y2="17" />
-                      </svg>
-                    </S.MenuDeleteButton>
+                    <ModalMenuDeleteButton
+                      deleteUserMenu={() => deleteUserMenu("userSoup")}
+                    />
                   </td>
                 )}
               </tr>
@@ -102,27 +67,9 @@ export default function MenuModal({ date, setIsOpenMenuModal }: MenuModalType) {
                 <td>{dayMenuList.userSideMenu.join(", ")}</td>
                 {dayMenuList.userSideMenu.length > 0 && (
                   <td>
-                    <S.MenuDeleteButton
-                      onClick={() => deleteUserMenu("userSideMenu")}
-                    >
-                      <svg
-                        className="feather feather-trash-2"
-                        fill="none"
-                        height="18"
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        viewBox="0 0 24 24"
-                        width="18"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <polyline points="3 6 5 6 21 6" />
-                        <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
-                        <line x1="10" x2="10" y1="11" y2="17" />
-                        <line x1="14" x2="14" y1="11" y2="17" />
-                      </svg>
-                    </S.MenuDeleteButton>
+                    <ModalMenuDeleteButton
+                      deleteUserMenu={() => deleteUserMenu("userSideMenu")}
+                    />
                   </td>
                 )}
               </tr>
@@ -193,17 +140,6 @@ const S = {
       :nth-child(3) {
         text-align: center;
       }
-    }
-  `,
-  MenuDeleteButton: styled.button`
-    font-size: 13px;
-    text-align: right;
-    border-radius: 15px;
-    color: #aaaaaa;
-    transition: 0.1s ease-in-out;
-    :hover {
-      transition: 0.1s ease-in-out;
-      color: black;
     }
   `,
 };
