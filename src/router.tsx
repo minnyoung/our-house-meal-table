a66bb297a15480716fada5512a139b6907e1f8fe
minnyoung <918liter@gmail.com>
2023-06-15 23:39:12 +0900

    feat: Not Found route 연결

--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import MealTable from "./pages/MealTable";
 import SignUp from "./pages/SignUp";
+import NotFound from "./pages/NotFound";
 
 export default function Router() {
   return (
@@ -10,6 +11,7 @@ export default function Router() {
         <Route path="/" element={<Home />} />
         <Route path="/signUp" element={<SignUp />} />
         <Route path="/mealTable" element={<MealTable />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
