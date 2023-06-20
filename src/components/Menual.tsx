4b685263426b74cf8c205558f3133ea51444ca57
minnyoung <918liter@gmail.com>
2023-06-20 23:47:21 +0900

    fix: Menual 모달 내 체크박스 수정(#11) - 체크박스에 따라 localStorage 값이 변할 수 있도록 수정했습니다.

--- src/components/Menual.tsx
+++ src/components/Menual.tsx
@@ -1,20 +1,30 @@
 import styled from "styled-components";
 import { setDisplayManual } from "../utils/manualFunction";
+import { useState } from "react";
 
 type MenualPropType = {
   setIsDisplayMenualState: React.Dispatch<React.SetStateAction<string | null>>;
 };
 export default function Menual({ setIsDisplayMenualState }: MenualPropType) {
+  const [isChecked, setIsChecked] = useState(false);
   return (
     <S.Container>
       <div>메뉴얼</div>
       <input
         type="checkbox"
         id="stopDisplayCheckbox"
-        onClick={() => setDisplayManual("false")}
+        checked={isChecked}
+        onClick={() => setIsChecked((previousValue) => !previousValue)}
       />
       <label htmlFor="stopDisplayCheckbox">이제 그만 볼게요</label>
-      <button onClick={() => setIsDisplayMenualState("false")}>닫기</button>
+      <button
+        onClick={() => {
+          if (isChecked) setDisplayManual("false");
+          setIsDisplayMenualState("false");
+        }}
+      >
+        닫기
+      </button>
     </S.Container>
   );
 }
