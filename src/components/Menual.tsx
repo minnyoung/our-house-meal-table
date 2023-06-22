de8122be872a96f96f91b3b08757052338f2246c
minnyoung <918liter@gmail.com>
2023-06-23 02:02:33 +0900

    feat: Menual gif 이미지 적용, 메뉴얼 내용 일부 추가, css 수정(#11)

--- src/components/Menual.tsx
+++ src/components/Menual.tsx
@@ -2,6 +2,12 @@ import styled from "styled-components";
 import { setDisplayManual } from "../utils/manualFunction";
 import { useState } from "react";
 import testPicture from "../assets/test.png";
+import menuModal from "../assets/menuModal.gif";
+import menuRecipe from "../assets/menuRecipe.gif";
+import menuRegist from "../assets/menuRegist.gif";
+import menuRemove from "../assets/menuRemove.gif";
+import menuSave from "../assets/menuSave.gif";
+import menuSearch from "../assets/menuSearch.gif";
 
 type MenualPropType = {
   setIsDisplayMenualState: React.Dispatch<React.SetStateAction<string | null>>;
@@ -15,7 +21,7 @@ export default function Menual({ setIsDisplayMenualState }: MenualPropType) {
         <S.SlideUl pageNumber={pageNumber}>
           <S.SlideLI>
             <span>메뉴 등록</span>
-            <img src={testPicture} alt="메뉴등록" />
+            <img src={menuRegist} alt="메뉴등록" />
             <p>
               오른쪽에 있는 메뉴를 원하는 날짜의 칸으로 드래그하면 메뉴가
               등록됩니다.
@@ -24,25 +30,38 @@ export default function Menual({ setIsDisplayMenualState }: MenualPropType) {
           </S.SlideLI>
           <S.SlideLI>
             <span>메뉴 저장</span>
-            <img src={testPicture} alt="메뉴저장" />
+            <img src={menuSave} alt="메뉴저장" />
             <p>메뉴를 등록하고 삭제한 후에는 메뉴저장 버튼을 꼭 눌러주세요.</p>
           </S.SlideLI>
           <S.SlideLI>
             <span>메뉴 삭제</span>
-            <img src={testPicture} alt="메뉴삭제" />
+            <img src={menuModal} alt="메뉴삭제" />
             <p>
               달력 칸을 클릭하면 등록한 메뉴들의 상세정보를 볼 수 있고 <br />
               메뉴의 전체 삭제와 개별 삭제가 가능합니다.
+              <p>(메뉴 삭제 후 반드시 메뉴 저장버튼을 눌러주세요!)</p>
+            </p>
+          </S.SlideLI>
+          <S.SlideLI>
+            <span>메뉴 삭제</span>
+            <img src={menuRemove} alt="메뉴삭제" />
+            <p>
+              달력 상단의 메뉴 초기화 버튼을 통해 해당 달의 메뉴를 전부 초기화
+              할 수 있습니다.
+              <p>(메뉴 초기화 후 반드시 메뉴 저장버튼을 눌러주세요!)</p>
             </p>
           </S.SlideLI>
           <S.SlideLI>
             <span>메뉴 검색</span>
-            <img src={testPicture} alt="메뉴검색" />
-            <p>원하는 메뉴 검색이 가능합니다.</p>
+            <img src={menuSearch} alt="메뉴검색" />
+            <p>
+              원하는 메뉴 검색이 가능합니다. <br />
+              검색된 메뉴를 달력에 등록해보세요!
+            </p>
           </S.SlideLI>
           <S.SlideLI>
             <span>메뉴 레시피 검색페이지</span>
-            <img src={testPicture} alt="메뉴레시피보기" />
+            <img src={menuRecipe} alt="메뉴레시피보기" />
             <p>
               등록한 메뉴를 클릭하면 레시피를 검색할 수 있는 페이지로
               이동합니다.
@@ -52,7 +71,7 @@ export default function Menual({ setIsDisplayMenualState }: MenualPropType) {
         <S.ArrowButton
           direction="left"
           onClick={() => {
-            if (pageNumber > 0 && pageNumber <= 2000)
+            if (pageNumber > 0 && pageNumber <= 2500)
               setPageNumber((previousValue) => previousValue - 500);
           }}
         >
@@ -61,7 +80,7 @@ export default function Menual({ setIsDisplayMenualState }: MenualPropType) {
         <S.ArrowButton
           direction="right"
           onClick={() => {
-            if (pageNumber >= 0 && pageNumber < 2000)
+            if (pageNumber >= 0 && pageNumber < 2500)
               setPageNumber((previousValue) => previousValue + 500);
           }}
         >
@@ -122,6 +141,7 @@ const S = {
     height: 500px;
     display: flex;
     flex-direction: column;
+    justify-content: center;
     flex: none;
     span {
       margin: 10px 0;
